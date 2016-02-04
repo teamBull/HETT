7f6f15b2e335c6f3609c925c472e2ec2cb603405
seongbong jang <jangsb7113@naver.com>
2016-02-04 22:14:33 +0900

    commit test

--- app/src/androidTest/java/com/teambulldozer/hett/ApplicationTest.java
+++ app/src/androidTest/java/com/teambulldozer/hett/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.teambulldozer.hett;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-
+//성봉주석 test
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
