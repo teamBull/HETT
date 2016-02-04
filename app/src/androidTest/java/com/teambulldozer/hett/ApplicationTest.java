ab38a61cdfbfed4125e2b254c4d2028fb8850bc8
seongbong jang <jangsb7113@naver.com>
2016-02-04 22:45:30 +0900

    comment delete

--- app/src/androidTest/java/com/teambulldozer/hett/ApplicationTest.java
+++ app/src/androidTest/java/com/teambulldozer/hett/ApplicationTest.java
@@ -2,7 +2,7 @@ package com.teambulldozer.hett;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
-//성봉 주석 추가
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
