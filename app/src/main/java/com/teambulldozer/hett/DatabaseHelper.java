04925de915522ac34dc3eeecc663c97e1e07c062
flecho <flecho@naver.com>
2016-02-17 22:00:54 +0900

    Delete function implemented, but optimization is needed.

--- app/src/main/java/com/teambulldozer/hett/DatabaseHelper.java
+++ app/src/main/java/com/teambulldozer/hett/DatabaseHelper.java
@@ -7,6 +7,7 @@ package com.teambulldozer.hett;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -15,12 +16,14 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final String DATABASE_NAME = "Event.db"; // case-insensitive -데이터베이스 이름.
-    public static final String TABLE_NAME = "event_table"; // 테이블명
+    public static final String DATABASE_NAME = "Event.db"; // case-insensitive
+    public static final String TABLE_NAME = "event_table";
 
     // There are only two columns
     public static final String COL_1 = "_id"; // Cursor 는 무조건 _ID가 있어야...
     public static final String COL_2 = "MEMO";
+    public static final String COL_3 = "IMPORTANCE";
+    public static final String COL_4 = "COMPLETENESS";
 
 
     public DatabaseHelper(Context context){
@@ -30,7 +33,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db){
         // And, if you want to create a table inside your DB,
-        db.execSQL("create table " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, MEMO TEXT);");
+        db.execSQL("create table " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, MEMO TEXT, IMPORTANCE TEXT, COMPLETENESS TEXT);");
+
     }
 
     @Override
@@ -38,52 +42,199 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
-    /**
-     * event table에 data를 inesrt하는 메소드.
-     * @param memo insert할 Memo 내용.
-     * @return insert 성공시 ture 리턴. 그 외엔 -1 리턴.
-     */
-    public boolean insertData(String memo){
+
+    public boolean insertData(String memo, boolean completeness){
         SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL_2, memo);
-        //DB에 insert하기 위해서는 ContentValues 객체가 필요하다. ContentValues- hash로 이루어짐.
+        contentValues.put(COL_3, "false"); // 입력 단계에서 중요 설정 할 수 없으므로, default 값은 false.
+
+        if(completeness)
+            contentValues.put(COL_4, "1"); // 입력 단계에서 완수여부 마찬가지로 설정할 수 없음.
+        else
+            contentValues.put(COL_4, "0"); // 입력 단계에서 완수여부 마찬가지로 설정할 수 없음.
+
         long result = db.insert(TABLE_NAME, null, contentValues); // takes 3 arguments..
         if(result == -1)
             return false;
+        else
+            return true;
+    }
+
+    public Cursor getCompletenessData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = { COL_1, COL_4 };
+        Cursor cursor = db.query(TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+
+    public boolean isThereCompletedData(){
+        Cursor cursor = getCompletenessData();
+        cursor.moveToFirst();
+
+        try {
+            do {
+                if(cursor.getString(1).equals("1")) // "1" means it is completed.
+                    return true;
+                else
+                    continue;
+            } while (cursor.moveToNext());
+        } finally {
+
+            if(cursor != null)
+                cursor.close();
+
+        }
+        return false;
+    }
+
+    // 커서는 id와 별개로 움직이나?
+    public boolean isCompleted(int position){ // maybe position_sync?
+        Cursor cursor = getCompletenessData();
+        cursor.moveToFirst();
+
+        try {
+            do {
+                if (Integer.toString(position).equals(cursor.getString(0))) {
+                    if(cursor.getString(1).equals("1")) // "1" means it is completed.
+                        return true;
+                    else
+                        return false;
+                }
+
+            } while (cursor.moveToNext());
+
+        } finally {
+            if(cursor != null)
+                cursor.close();
+        }
+
+
+        return false;
+    }
+
+
+    public Cursor getMemoData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = { COL_1, COL_2 };
+        Cursor cursor = db.query(TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+
+    public String getMemoAt(int position){
+        Cursor cursor = getMemoData();
+        cursor.moveToFirst();
+        try{
+            do {
+                if (Integer.toString(position).equals(cursor.getString(0)))
+                    return cursor.getString(1);
+
+            } while (cursor.moveToNext());
+        } finally {
+            if(cursor != null)
+                cursor.close();
+        }
+
+        return "Error";
+    }
+
+    public Cursor getImportanceData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] columns = { COL_1, COL_3 };
+        Cursor cursor = db.query(TABLE_NAME, columns, null, null, null, null, null);
+        return cursor;
+    }
+
+
+    public boolean updateData(String id, String memo, String importance, String completeness){
+        SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
+        ContentValues values = new ContentValues();
+        values.put(COL_1, id);
+        //values.put(COL_2, memo);
+        values.put(COL_3, importance);
+        values.put(COL_4, completeness);
+        db.update(TABLE_NAME, values, " _id = ?", new String[] { id });
+
         return true;
     }
 
-    /**
-     * event table의 한 row를 delete하는 메소드.
-     * @param id event_table의 삭제할 primary key number.
-     * @return 삭제된 컬럼 갯수.
-     */
     public Integer deleteData(String id){ // Since id is a primary key
-        //getWriteableDatabase는 database를 write하기 위하여 SQLiteDatabase객체를 retrun하는것.
         SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
         return db.delete(TABLE_NAME, "_id = ?", new String[] { id });
     }
 
-    /**
-     * 테이블의 한 튜플(row)가 삭제될 때, id(event_table)의 number를 하나 낮춰서 update.
-     * @param id 삭제될 database의 primary key number.
-     */
-    public void rearrangeData(String id){
+    public void deleteAllData(){
         SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
-        db.execSQL("UPDATE " + TABLE_NAME + " SET _id = (_id - 1) WHERE _id > " + id); // 삭제되는 id보다 높은 번호들을 다 -1해서 저장.
-        db.execSQL("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='event_table'"); // 시퀀스 넘버를 0으로 초기화..???????
+        db.execSQL("delete from " + TABLE_NAME);
     }
 
-    /**
-     * event_table의 모든 row를 Cursor로 return하는 메소드.
-     * @return event_table의 모든 row정보가 담긴 Cursor
-     */
+    public void rearrangeData(String id){
+        SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
+        db.execSQL("UPDATE " + TABLE_NAME + " SET _id = (_id - 1) WHERE _id > " + id);
+        db.execSQL("UPDATE SQLITE_SEQUENCE SET SEQ=0 WHERE NAME='event_table'");
+    }
+
+    public void moveDataToTop(String memo){
+        SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
+        ContentValues values = new ContentValues();
+        values.put(COL_2, memo);
+        values.put(COL_3, "false");
+        values.put(COL_4, "0");
+        db.update(TABLE_NAME, values, " _id = ?", new String[]{"1"});
+    }
+
+
+    public void shiftData(int position){
+
+        for(; position >= 1; position--){
+            shiftData_aux(position);
+        }
+    }
+
+
+    public void shiftData_aux(int position){
+
+        SQLiteDatabase db_read = this.getReadableDatabase();
+        String[] columns = {COL_1, COL_2, COL_3, COL_4};
+        Cursor cursor = db_read.query(TABLE_NAME, columns, null, null, null, null, null);
+        cursor.moveToFirst();
+
+        try {
+
+            do {
+                if (Integer.toString(position).equals(cursor.getString(0))) {
+                    SQLiteDatabase db_write = this.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+                    values.put(COL_2, cursor.getString(1));
+                    values.put(COL_3, cursor.getString(2));
+                    values.put(COL_4, cursor.getString(3));
+                    String id = Integer.toString(position + 1);
+                    db_write.update(TABLE_NAME, values, " _id = ?", new String[]{id});
+
+                }
+
+            } while (cursor.moveToNext());
+        } finally {
+            if(cursor != null)
+                cursor.close(); // Error doesn't occur.
+        }
+
+    }
+
     public Cursor getAllData(){
         SQLiteDatabase db = this.getWritableDatabase(); // It is going to create your database and table.
         Cursor res = db.rawQuery("select rowid _id, * from " + TABLE_NAME, null);
         // SQLiteDatabase class provides result sets as Cursor instances.
         return res;
-
+        // Cursor가 하는 일이 뭐야?
+        // Cursor를 이용하면 우리의 데이터에 접근할 수 있음.
     }
+
+    public long numOfEntries()
+    {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return DatabaseUtils.queryNumEntries(db, TABLE_NAME);
+    }
+
+
 }
--- app/src/main/java/com/teambulldozer/hett/MainActivity.java
+++ app/src/main/java/com/teambulldozer/hett/MainActivity.java
@@ -1,103 +1,426 @@
 package com.teambulldozer.hett;
 
+
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static boolean devMode = false; // 이게 트루로 설정이 되어 있어서 튕기는 것일 수도 있음.
     private static final String TAG = "HETT";
 
     DatabaseHelper myDb;
+    MySimpleCursorAdapter mySimpleCursorAdapter;
     ListView lv1;
     EditText memoInput;
-    Button addButton;
+    TextView addButton;
+    TextView dateBar;
+    TextView todayBar;
+    TextView editMenu;
+    ImageView addLine;
+    RelativeLayout rl1;
+    RelativeLayout rl2;
+    LinearLayout dateLayout;
+    SoftKeyboardLsnedRelativeLayout myLayout;
+
+    //편집 버튼을 눌렀을 때 VISIBLE하게 되는 위젯들
+    TextView finishMenu;
+    ImageView deleteButton;
+    ImageView orderButton;
+    TextView deleteAllButton;
+
+    // 폰트
+    Typeface NanumSquare_B;
+    Typeface NanumBarunGothic_R;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (devMode)
+        {
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_main);
 
+        /* Call the database constructor */
         myDb = new DatabaseHelper(this); // going to call the constructor
-        lv1 = (ListView) findViewById(R.id.list_view1);
 
+        /* Connecting XML widgets and JAVA code. */
+        dateBar = (TextView) findViewById(R.id.dateBar);
+        todayBar = (TextView) findViewById(R.id.todayBar);
+        editMenu = (TextView) findViewById(R.id.editMenu);
+        lv1 = (ListView) findViewById(R.id.lv1);
         memoInput = (EditText) findViewById(R.id.memoInput);
+        addButton = (TextView) findViewById(R.id.addButton);
+        addLine = (ImageView) findViewById(R.id.addLine);
+        rl1 = (RelativeLayout) findViewById(R.id.rl1);
+        rl2 = (RelativeLayout) findViewById(R.id.rl2);
+        dateLayout = (LinearLayout) findViewById(R.id.dateLayout);
+        myLayout = (SoftKeyboardLsnedRelativeLayout) findViewById(R.id.myLayout);
+
+        /* 편집 눌렀을 때 추가 되는 버튼들 */
+        finishMenu = (TextView) findViewById(R.id.finishMenu);
+        deleteButton = (ImageView) findViewById(R.id.deleteButton);
+        orderButton = (ImageView) findViewById(R.id.orderButton);
+        deleteAllButton = (TextView) findViewById(R.id.deleteAllButton);
+
+
+
+        /* Font Initialization */
+        NanumSquare_B = Typeface.createFromAsset(getAssets(), "NanumSquare_Bold.ttf");
+        NanumBarunGothic_R = Typeface.createFromAsset(getAssets(), "NanumBarunGothic_Regular.ttf");
+
+
+        /* These are the main functions of the main page. */
+        setFont(); // 폰트 설정
+        showDate(); // 날짜 보여주기
+        respondToUserInput(); // 유저가 타이핑을 시작하면, addButton이 뜨게 한다!
+        screenDownOnNormalState();
+        screenUpOnUserType(); // 유저가 edittext widget을 클릭하면, 메뉴 바가 사라진다.
+
+        ifEditMenuClicked();
+        ifFinishMenuClicked();
+//        deleteIfItemClicked(); // 유저가 삭제 버튼을 누르면 삭제하기.
+
+        addOnUserInput(); // 유저가 입력하면 받기
+        completeIfItemClicked(); // 아이템 클릭되면 View 바꿔주기
+        //deleteIfItemClicked(); // 아이템 클릭되면 지우기
+        ifClickedDeleteAllRows(); // 모두 지우기 버튼이 눌렀을 때, 일정 모두 지우기.
+        populateListView(); // 리스트뷰에 아이템 올리기
+
+        /* Additional details */
+
+        // The following line makes software keyboard disappear until it is clicked again.
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 
-        addButton = (Button) findViewById(R.id.addButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
+    }
+
+    public void ifEditMenuClicked(){
+
+        editMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                boolean isInserted = myDb.insertData(memoInput.getText().toString());
-                if (isInserted) {
-                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
-                    memoInput.setText("");
-                    /*
-                    * These lines are added for users to easily use EditText Widget.
-                      After user input data, the software keyboard disappeared and, text in EditText
-                      are set to "".
-                    * */
-                    populateListView();
 
-                } else
-                    Toast.makeText(MainActivity.this, "Data Not Inserted", Toast.LENGTH_LONG).show();
+                editMenu.setVisibility(View.INVISIBLE);
+                finishMenu.setVisibility(View.VISIBLE);
+
+                MySimpleCursorAdapter.isOnEditMenu = false;
+                ((BaseAdapter)mySimpleCursorAdapter).notifyDataSetChanged();
+                //populateListView(); // 대체
+                rl2.setVisibility(View.INVISIBLE);
+                deleteAllButton.setVisibility(View.VISIBLE);
+
+                // 어댑터에 옵션 걸어줘서 다시 populateListView 해야함..
+
+
+            }
+        });
+    }
+
+    public void ifFinishMenuClicked(){
+
+        finishMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finishMenu.setVisibility(View.INVISIBLE);
+                editMenu.setVisibility(View.VISIBLE);
+
+                MySimpleCursorAdapter.isOnEditMenu = true;
+                ((BaseAdapter)mySimpleCursorAdapter).notifyDataSetChanged();
+                //populateListView();
+                rl2.setVisibility(View.VISIBLE);
+                deleteAllButton.setVisibility(View.INVISIBLE);
+                // 어댑터에 옵션 걸어줘서 다시 populateListView 해야함..
+
+            }
+        });
+    }
+
+    public void showDate(){
+
+        Calendar rightNow = Calendar.getInstance();
+        // Calendar's getInstance method returns a calendar whose locale is based on system settings
+        // and whose time fields have been initialized with the current date and time.
+
+        int monthInfo = rightNow.get(Calendar.MONTH);
+        int dateInfo = rightNow.get(Calendar.DATE);
+        int dayInfo = rightNow.get(Calendar.DAY_OF_WEEK);
+        int ampmInfo = rightNow.get(Calendar.AM_PM);
+        int hourInfo = rightNow.get(Calendar.HOUR);
+        int minuteInfo = rightNow.get(Calendar.MINUTE);
+
+        String KOR_DAY = dayConverter(dayInfo);
+        String KOR_AMPM = ampmConverter(ampmInfo);
+        String KOR_MINUTE = minuteConverter(minuteInfo);
+        dateBar.setText(monthInfo + "월 " + dateInfo + "일 " + KOR_DAY + " " + KOR_AMPM + " " + hourInfo + ":" + KOR_MINUTE);
+        // need to convert Eng-based day to Kor-based day.
+
+    }
+
+    public void respondToUserInput(){
+        memoInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addButton.setVisibility(View.VISIBLE);
+                addLine.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length() == 0) {
+                       /* After a user types some text, a button disappear.*/
+                    addButton.setVisibility(View.INVISIBLE);
+                    addLine.setVisibility(View.INVISIBLE);
+                }
 
             }
         });
 
-        populateListView();
+    }
+
+    public void screenDownOnNormalState(){
+        myLayout.addSoftKeyboardLsner(new SoftKeyboardLsnedRelativeLayout.SoftKeyboardLsner() {
+            @Override
+            public void onSoftKeyboardShow() {
+                Log.d("SoftKeyboard", "Soft keyboard shown");
+            }
+
+            @Override
+            public void onSoftKeyboardHide() {
+                Log.d("SoftKeyboard", "Soft keyboard hidden");
+                MySimpleCursorAdapter.isUserOnTyping = false;
+
+                rl1.setVisibility(View.VISIBLE);
+
+                ViewGroup.MarginLayoutParams LL = (ViewGroup.MarginLayoutParams) dateLayout.getLayoutParams();
+                LL.topMargin = pixelToDP(55);
+
+                ViewGroup.MarginLayoutParams LV = (ViewGroup.MarginLayoutParams) lv1.getLayoutParams();
+                LV.topMargin = pixelToDP(137);
+
+            }
+        });
+
+
+    }
+
+    public void screenUpOnUserType(){
+        memoInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rl1.setVisibility(View.INVISIBLE);
+
+                ViewGroup.MarginLayoutParams LL = (ViewGroup.MarginLayoutParams) dateLayout.getLayoutParams();
+                LL.topMargin = pixelToDP(40);
+
+                ViewGroup.MarginLayoutParams LV = (ViewGroup.MarginLayoutParams) lv1.getLayoutParams();
+                LV.topMargin = pixelToDP(122); //96
+
+                MySimpleCursorAdapter.isUserOnTyping = true;
+                ((BaseAdapter)mySimpleCursorAdapter).notifyDataSetChanged();
+                //populateListView();
+
+            }
+        });
+    }
+
+    public int pixelToDP(int paddingPixel){
+        float density = getBaseContext().getResources().getDisplayMetrics().density;
+        int paddingDp = (int)(paddingPixel * density);
+        return paddingDp;
+    }
+
+    public void setFont(){
+        dateBar.setTypeface(NanumSquare_B);
+        todayBar.setTypeface(NanumSquare_B);
+        memoInput.setTypeface(NanumSquare_B);
+        editMenu.setTypeface(NanumSquare_B);
+        addButton.setTypeface(NanumSquare_B);
+        finishMenu.setTypeface(NanumSquare_B);
+        deleteAllButton.setTypeface(NanumSquare_B);
+
+    }
+
+    public String minuteConverter(int minuteInfo){
+        String formatted = String.format("%02d", minuteInfo);
+        return formatted;
+    }
+
+    public String ampmConverter(int ampmInfo){
+        if(ampmInfo == 0)
+            return "AM";
+        else
+            return "PM";
+    }
+
+    public String dayConverter(int dayInfo){
+        switch(dayInfo){
+            case 1:
+                return "(일)";
+            case 2:
+                return "(월)";
+            case 3:
+                return "(화)";
+            case 4:
+                return "(수)";
+            case 5:
+                return "(목)";
+            case 6:
+                return "(금)";
+            case 7:
+                return "(토)";
+        }
+        return "Error";
+    }
+
+    public void completeIfItemClicked(){
         lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Cursor cursor = (Cursor) parent.getAdapter().getItem(position);
                 cursor.moveToPosition(position);
                 int rowId = (cursor.getPosition() + 1); // sqlite와 sync를 맞춰줘야함.
-                Toast.makeText(MainActivity.this, "Item Clicked " + rowId, Toast.LENGTH_LONG).show();
-                if (deleteRow(Integer.toString(rowId)))
-                    Toast.makeText(MainActivity.this, "Data Deleted", Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(MainActivity.this, "Data Not Deleted", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getBaseContext(), "Item Clicked: " + rowId, Toast.LENGTH_SHORT).show();
 
+                if(myDb.isCompleted(rowId)) {
+                    shiftAndInsert(rowId);
+                    // 이 라인에 추가되어야 할 코드는 디비에 있는 내용을 한 칸씩 다음뷰로 shift하는 것임.
+                }
+                else {
+                    deleteAndInsert(rowId);
+                }
 
+                //((BaseAdapter)mySimpleCursorAdapter).notifyDataSetChanged();
+                // 이 라인은 교체하면 에러가 뜬다...
+                populateListView();
             }
         });
-        ((BaseAdapter) lv1.getAdapter()).notifyDataSetChanged();
+    }
+
+    public void ifClickedDeleteAllRows(){
+        deleteAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDb.deleteAllData();
+                myDb.rearrangeData(Integer.toString((int) myDb.numOfEntries()));
+                // 삭제와 관련된 부분은 adapter를 새로 설정해줘야함;
+                populateListView();
+            }
+        });
 
     }
 
     public boolean deleteRow(String rowId){
-        //Toast.makeText(MainActivity.this, "deleteRow Entered", Toast.LENGTH_LONG).show();
-        // 위의 토스트는 디버그용..
+
         Integer deletedRows = myDb.deleteData(rowId);
         myDb.rearrangeData(rowId);
-        populateListView(); // 데이터에 변화가 생겼기 때문에, 새롭게 어댑터를 다시 설정해준다.
-        //Toast.makeText(MainActivity.this, "delete Succeeded", Toast.LENGTH_LONG).show();
+        populateListView();
         return deletedRows != 0;
     }
 
 
+    public void shiftAndInsert(int rowId){
+        String tempMemo = myDb.getMemoAt(rowId);
+
+        if(rowId == 1){
+            myDb.moveDataToTop(tempMemo); // Just update the value at position 1.
+        } else {
+            myDb.shiftData(rowId-1); // The reason for rowId-1 is for shifting.
+            myDb.moveDataToTop(tempMemo); // 순서 굉장히 중요!
+        }
+    }
+
+    public void deleteAndInsert(int rowId){
+
+        String tempMemo = myDb.getMemoAt(rowId);
+        deleteRow(Integer.toString(rowId));
+        myDb.insertData(tempMemo, true);
+
+    }
+
+    public void addOnUserInput() {
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String memo = memoInput.getText().toString();
+                if (memo.isEmpty()) {
+                    return;
+                    /* If a user does not type any word, then, memo is not to be added to the list. */
+                }
+
+                //myDb.getReadableDatabase().
+
+                if (myDb.numOfEntries() == 0) { // 이 조건이 있는 이유는, DB가 비어있을 때, isThereCompleteData()를 호출하면 에러가 뜨기 때문.
+                    myDb.insertData(memo, false);
+                } else {
+                    if (myDb.isThereCompletedData()) {
+                        myDb.insertData("", false);
+                        myDb.shiftData((int) myDb.numOfEntries()); // 여기 왜 1이 들어가있지.. 마지막 숫자가 들어가 있어야되는데 ;
+                        myDb.moveDataToTop(memo);
+                    } else
+                        myDb.insertData(memo, false);
+                }
+
+                    /*
+                    * These lines makes a software keyboard disappear when user finishes typing.
+                    * */
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                memoInput.setText("");
+
+                populateListView();
+            }
+        });
+    }
+
     private void populateListView(){
         Cursor cursor = myDb.getAllData();
-        String[] fromFieldNames = new String[] { myDb.COL_1, myDb.COL_2 };
-        int[] toViewIDS = new int[] {R.id.memoIndex, R.id.memoContent};
-        SimpleCursorAdapter myCursorAdapter;
-        myCursorAdapter = new SimpleCursorAdapter(getBaseContext(), R.layout.list_item_memo, cursor, fromFieldNames, toViewIDS, 0);
-        lv1.setAdapter(myCursorAdapter);
+        String[] fromFieldNames = new String[] { myDb.COL_2 };
+        int[] toViewIDS = new int[] { R.id.memoContent};
+        mySimpleCursorAdapter =
+                new MySimpleCursorAdapter(this, R.layout.list_item_memo, cursor, fromFieldNames, toViewIDS, 0, myDb);
+        lv1.setAdapter(mySimpleCursorAdapter);
+        // getBaseContext --> to this because of cast problem.
     }
 
     /*
@@ -129,6 +452,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onDestroy(){
         super.onDestroy();
+        ((SimpleCursorAdapter)lv1.getAdapter()).getCursor().close(); // ?
+        myDb.close();
         Log.d(TAG, "onDestroy() called");
     }
 
