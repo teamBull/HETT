c11fc5a139f881cc1909f0d4bfc73bdae208faf1
yunsunChoe <qoflqot@naver.com>
2016-02-27 04:21:30 +0900

    yeeeee

--- app/src/main/java/com/teambulldozer/hett/AlarmAMZeroReceiver.java
+++ app/src/main/java/com/teambulldozer/hett/AlarmAMZeroReceiver.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 /**
  * Created by GHKwon on 2016-02-27.
@@ -14,8 +15,8 @@ public class AlarmAMZeroReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context,Intent intent) {
         //여따가 12시 되면 해야될 것들 작성해라.
-        double todayPoint;
-        double totalPoint;
+        double todayPoint = 0;
+        double totalPoint = 0;
         EventTableController eventTableControllerr;
         FriendDataManager friendDataManager;
 
@@ -31,6 +32,8 @@ public class AlarmAMZeroReceiver extends BroadcastReceiver {
         //FriendDatamanager에서 점수 불러옥 오늘 점수를 더해준 후 없뎃
         totalPoint = friendDataManager.getTotalPoint()+todayPoint;
         friendDataManager.updateTotalPoint(1, totalPoint);
+
+        Log.i("d", totalPoint + "");
 
 
     }
