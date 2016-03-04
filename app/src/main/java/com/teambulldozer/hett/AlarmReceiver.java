bdf41a1ecd5dcb0b9509aed1c8cd9133bff05322
yoon <foryou8904@naver.com>
2016-03-04 17:28:01 +0900

    Sync with main page

--- app/src/main/java/com/teambulldozer/hett/AlarmReceiver.java
+++ app/src/main/java/com/teambulldozer/hett/AlarmReceiver.java
@@ -7,8 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import java.util.Calendar;
-
 /**
  * Created by yoon on 16. 2. 22..
  */
@@ -23,8 +21,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         if (extra != null) {
             Log.i("alarmOnReceive", "extra has values");
 
-            boolean[] week = extra.getBooleanArray("days");
-
+            boolean[] week = extra.getBooleanArray("alarm_days");
+            /* 반복요일을 DB에 넘겨준 후에 주석을 풀어서 확인
             for(int i = 0; i < 8; i++) {
                 Log.i("alarmOnReceive", "week[" + Integer.toString(i)+ "] = " + Boolean.toString(week[i]));
             }
@@ -33,7 +31,7 @@ public class AlarmReceiver extends BroadcastReceiver {
 
             if (!week[cal.get(Calendar.DAY_OF_WEEK)])
                 return;
-
+            */
             // 화면 호출
             Log.i("alarmOnReceive", "화면 호출");
             Intent OnTriggerIntent = new Intent(context, AlarmOnTrigger.class);
